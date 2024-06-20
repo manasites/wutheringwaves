ad5845595bd0ddf221d1562a00fb0769eeef9d3f
xHomu <nobisan@gmail.com>
2024-06-20 14:04:05 -0700

    fix

--- app/_custom/routes/_site.convene-tracker.($convene)/DatesChart.tsx
+++ app/_custom/routes/_site.convene-tracker.($convene)/DatesChart.tsx
@@ -71,7 +71,6 @@ function generateDatesLabels({
    dates: Record<string, number>;
    filters: WuwaFiltersType;
 }) {
-   console.log(filters);
    let dateArray = Object.keys(dates);
    let startDate = filters?.startDate || dateArray[0]!;
    let endDate = filters?.endDate || dateArray[dateArray.length! - 1]!;
@@ -93,4 +92,55 @@ function generateDatesLabels({
       console.log(error);
       return dateArray;
    }
+}
+
+export function DateFilters({
+   versions,
+   filters,
+   setFilters,
+}: {
+   versions: { version: string; startDate: string; endDate: string }[];
+   filters: WuwaFiltersType;
+   setFilters: (filters: WuwaFiltersType) => void;
+}) {
+   return (
+      <div>
+         <input
+            type="date"
+            name="startDate"
+            className="dark:text-zinc-100 px-1 dark:bg-dark400 bg-zinc-50  shadow-sm dark:shadow-zinc-800/70 border-zinc-200/70
+      font-header  text-lg  rounded-l rounded-r-md overflow-hidden border shadow-zinc-50 dark:border-zinc-700 "
+            value={filters.startDate}
+            onChange={(e) =>
+               setFilters({ ...filters, startDate: e.target.value })
+            }
+         />
+         to
+         <input
+            type="date"
+            name="endDate"
+            className="dark:text-zinc-100 px-1 dark:bg-dark400 bg-zinc-50  shadow-sm dark:shadow-zinc-800/70 border-zinc-200/70
+font-header  text-lg  rounded-l rounded-r-md overflow-hidden border shadow-zinc-50 dark:border-zinc-700 "
+            value={filters.endDate}
+            onChange={(e) =>
+               setFilters({ ...filters, endDate: e.target.value })
+            }
+         />
+         {versions.map((v) => (
+            <button
+               key={v.version}
+               className="relative isolate inline-flex items-center justify-center gap-x-2 rounded-lg border text-base/6 font-semibold px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] tablet:px-[calc(theme(spacing.3)-1px)] tablet:py-[calc(theme(spacing[1.5]))] tablet:text-tablet/6 focus:outline-none data-[focus]:outline data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-blue-500 data-[disabled]:opacity-50 [&>[data-slot=icon]]:-mx-0.5 [&>[data-slot=icon]]:my-0.5 [&>[data-slot=icon]]:size-5 [&>[data-slot=icon]]:shrink-0 [&>[data-slot=icon]]:text-[--btn-icon] [&>[data-slot=icon]]:tablet:my-1 [&>[data-slot=icon]]:tablet:size-4 forced-colors:[--btn-icon:ButtonText] forced-colors:data-[hover]:[--btn-icon:ButtonText] border-zinc-950/10 text-zinc-950 data-[active]:bg-zinc-950/[2.5%] data-[hover]:bg-zinc-950/[2.5%] dark:border-white/15 dark:text-white dark:[--btn-bg:transparent] dark:data-[active]:bg-white/5 dark:data-[hover]:bg-white/5 [--btn-icon:theme(colors.zinc.500)] data-[active]:[--btn-icon:theme(colors.zinc.700)] data-[hover]:[--btn-icon:theme(colors.zinc.700)] dark:data-[active]:[--btn-icon:theme(colors.zinc.400)] dark:data-[hover]:[--btn-icon:theme(colors.zinc.400)] cursor-pointer"
+               onClick={() =>
+                  setFilters({
+                     ...filters,
+                     startDate: v.startDate,
+                     endDate: v.endDate,
+                  })
+               }
+            >
+               {v.version}
+            </button>
+         ))}
+      </div>
+   );
 }
--- app/_custom/routes/_site.convene-tracker.($convene)/GachaGlobal.tsx
+++ app/_custom/routes/_site.convene-tracker.($convene)/GachaGlobal.tsx
@@ -3,11 +3,9 @@ import { useState } from "react";
 import type { SerializeFrom } from "@remix-run/node";
 import { useRouteLoaderData } from "@remix-run/react";
 
-import { Button } from "~/components/Button";
-import { H2 } from "~/components/Headers";
 import { Image } from "~/components/Image";
 
-import { DatesChart } from "./DatesChart";
+import { DateFilters, DatesChart } from "./DatesChart";
 import { PitiesChart } from "./PitiesChart";
 import type { loader } from "./route";
 
@@ -16,6 +14,12 @@ type WuwaFiltersType = {
    endDate?: string;
    resourceId?: string;
 };
+
+// we'll hardcode the version dates for now
+const versions = [
+   { version: "v1.0", startDate: "2024-05-22", endDate: "2024-06-28" },
+   { version: "v1.1", startDate: "2024-06-28", endDate: "2024-07-25" },
+];
 
 export function GachaGlobal({
    summary,
@@ -72,7 +76,11 @@ export function GachaGlobal({
                </div>
             </div>
          </div>
-         <DateFilters filters={filters} setFilters={setFilters} />
+         <DateFilters
+            versions={versions}
+            filters={filters}
+            setFilters={setFilters}
+         />
          {dates && <DatesChart dates={dates} filters={filters} />}
          <FiveStars
             fiveStars={summary.fiveStars}
@@ -83,62 +91,6 @@ export function GachaGlobal({
          />
          {pities && <PitiesChart pities={pities} />}
          {/* {pities && <LineandBarChart pities={pities} />} */}
-      </div>
-   );
-}
-
-// we'll hardcode the version dates for now
-const versions = [
-   { version: "v1.0", startDate: "2024-05-22", endDate: "2024-06-28" },
-   { version: "v1.1", startDate: "2024-06-28", endDate: "2024-07-25" },
-];
-
-function DateFilters({
-   filters,
-   setFilters,
-}: {
-   filters: WuwaFiltersType;
-   setFilters: (filters: WuwaFiltersType) => void;
-}) {
-   return (
-      <div>
-         {versions.map((v) => (
-            <button
-               key={v.version}
-               className="relative isolate inline-flex items-center justify-center gap-x-2 rounded-lg border text-base/6 font-semibold px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] tablet:px-[calc(theme(spacing.3)-1px)] tablet:py-[calc(theme(spacing[1.5]))] tablet:text-tablet/6 focus:outline-none data-[focus]:outline data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-blue-500 data-[disabled]:opacity-50 [&>[data-slot=icon]]:-mx-0.5 [&>[data-slot=icon]]:my-0.5 [&>[data-slot=icon]]:size-5 [&>[data-slot=icon]]:shrink-0 [&>[data-slot=icon]]:text-[--btn-icon] [&>[data-slot=icon]]:tablet:my-1 [&>[data-slot=icon]]:tablet:size-4 forced-colors:[--btn-icon:ButtonText] forced-colors:data-[hover]:[--btn-icon:ButtonText] border-zinc-950/10 text-zinc-950 data-[active]:bg-zinc-950/[2.5%] data-[hover]:bg-zinc-950/[2.5%] dark:border-white/15 dark:text-white dark:[--btn-bg:transparent] dark:data-[active]:bg-white/5 dark:data-[hover]:bg-white/5 [--btn-icon:theme(colors.zinc.500)] data-[active]:[--btn-icon:theme(colors.zinc.700)] data-[hover]:[--btn-icon:theme(colors.zinc.700)] dark:data-[active]:[--btn-icon:theme(colors.zinc.400)] dark:data-[hover]:[--btn-icon:theme(colors.zinc.400)] cursor-pointer"
-               onClick={() =>
-                  setFilters({
-                     ...filters,
-                     startDate: v.startDate,
-                     endDate: v.endDate,
-                  })
-               }
-            >
-               {v.version}
-            </button>
-         ))}
-         Date Range:
-         <input
-            type="date"
-            name="startDate"
-            className="dark:text-zinc-100 px-1 dark:bg-dark400 bg-zinc-50  shadow-sm dark:shadow-zinc-800/70 border-zinc-200/70
-      font-header  text-lg  rounded-l rounded-r-md overflow-hidden border shadow-zinc-50 dark:border-zinc-700 "
-            value={filters.startDate}
-            onChange={(e) =>
-               setFilters({ ...filters, startDate: e.target.value })
-            }
-         />
-         to
-         <input
-            type="date"
-            name="endDate"
-            className="dark:text-zinc-100 px-1 dark:bg-dark400 bg-zinc-50  shadow-sm dark:shadow-zinc-800/70 border-zinc-200/70
-font-header  text-lg  rounded-l rounded-r-md overflow-hidden border shadow-zinc-50 dark:border-zinc-700 "
-            value={filters.endDate}
-            onChange={(e) =>
-               setFilters({ ...filters, endDate: e.target.value })
-            }
-         />
       </div>
    );
 }
